77281ea5ead4dce5e227af7844046a2f7a0cd1e7
Luca Sgroi <l.sgroi@marketinvoice.com>
2017-04-05 16:13:28 +0100

    Add solution with empty package

---
+++ .build/tasks/Test-PackageInstallation.ps1
@@ -0,0 +1,11 @@
+Import-Script Pask.Tests.Infrastructure -Package Pask
+
+# Synopsis: Test manually the package installation
+Task Test-PackageInstallation Clean, Pack-Nuspec, Push-Local, {
+    $Assertion = {
+        $PaskVersion = (([xml](Get-Content (Join-Path $ProjectFullPath "Pask.Test.nuspec"))).package.metadata.dependencies.dependency | Where { $_.id -eq "Pask" }).version
+        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application.Domain\packages.config"))).packages.package | Where { $_.id -eq "Pask" }).version -eq $PaskVersion) "Incorrect version of Pask installed into project 'Application.Domain'"
+    }
+
+    Test-PackageInstallation -Name Pask.Test -Assertion $Assertion -InstallationTargetInfo "Install into 'Application.Domain' project"
+}
