4b48a1fd3320cce5b9ad6447dddf10e4c8406398
Luca Sgroi <lucasgroi@gmail.com>
2017-04-17 11:22:11 +0100

    Update dependencies

--- .build/tasks/Test-PackageInstallation.ps1
+++ .build/tasks/Test-PackageInstallation.ps1
@@ -4,8 +4,8 @@ Import-Script Pask.Tests.Infrastructure -Package Pask
 Task Test-PackageInstallation Clean, Pack-Nuspec, Push-Local, {
     $Assertion = {
         $PaskVersion = (([xml](Get-Content (Join-Path $ProjectFullPath "Pask.Test.nuspec"))).package.metadata.dependencies.dependency | Where { $_.id -eq "Pask" }).version
-        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application.Domain\packages.config"))).packages.package | Where { $_.id -eq "Pask" }).version -eq $PaskVersion) "Incorrect version of Pask installed into project 'Application.Domain'"
+        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application\packages.config"))).packages.package | Where { $_.id -eq "Pask" }).version -eq $PaskVersion) "Incorrect version of Pask installed into project 'Application'"
     }
 
-    Test-PackageInstallation -Name Pask.Test -Assertion $Assertion -InstallationTargetInfo "Install into 'Application.Domain' project"
+    Test-PackageInstallation -Name Pask.Test -Assertion $Assertion -InstallationTargetInfo "Install into 'Application' project"
 }
